0a0c742f7a6649836a6c2c158dcfa3a25548a513
chinmoyi <cb849@scarletmail.rutgers.edu>
2017-12-02 20:03:15 -0500

    test list view for album names

--- Photos_Android43/app/src/main/java/com/photos_android/photos_android43/UserHomepage.java
+++ Photos_Android43/app/src/main/java/com/photos_android/photos_android43/UserHomepage.java
@@ -2,12 +2,26 @@ package com.photos_android.photos_android43;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.*;
+//import java.util.ArrayList;
 
 public class UserHomepage extends AppCompatActivity {
+    ListView testList;
+    ArrayList<String> albums = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        albums.add("Deep");
+        albums.add("Chinmoyi");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_homepage);
+        testList = (ListView) findViewById(R.id.albumsList);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.activity_albums_list_view,
+                R.id.textView,albums);
+        testList.setAdapter(arrayAdapter);
     }
 }
