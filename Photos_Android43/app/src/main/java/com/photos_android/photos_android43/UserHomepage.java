f1e8dbae82a1fa73941fb6ded6d4f31b1941c895
chinmoyi <cb849@scarletmail.rutgers.edu>
2017-12-04 19:14:43 -0500

    add album dialog box

--- Photos_Android43/app/src/main/java/com/photos_android/photos_android43/UserHomepage.java
+++ Photos_Android43/app/src/main/java/com/photos_android/photos_android43/UserHomepage.java
@@ -5,8 +5,13 @@ import android.support.annotation.XmlRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.LayoutInflater;
+import android.support.v7.app.AlertDialog;
+import android.widget.EditText;
+import android.content.DialogInterface;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -21,13 +26,15 @@ import model.PhotoAlbumManager;
 
 public class UserHomepage extends AppCompatActivity {
 
+    private FloatingActionButton addAlbumBtn;
+    final Context c = this;
+
     public static PhotoAlbumManager manager = new PhotoAlbumManager();
     File albumsfile = new File("/data/data/com.photos_android.photos_android43/files/albums.dat");
     ListView testList;
     //private static List<Album> albums = new ArrayList<Album>();
     private static List<String> albums = new ArrayList<String>();
 
-    FloatingActionButton addAlbumBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,37 @@ public class UserHomepage extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_homepage);
+
+        addAlbumBtn = (FloatingActionButton) findViewById(R.id.addAlbumBtn);
+        addAlbumBtn.setOnClickListener (new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
+                View mView = layoutInflaterAndroid.inflate(R.layout.activity_create_album_dialogbx, null);
+                AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
+                alertDialogBuilderUserInput.setView(mView);
+
+                final EditText userInputDialogEditText = (EditText) mView.findViewById(R.id.userInputDialog);
+                alertDialogBuilderUserInput
+                        .setCancelable(false)
+                        .setPositiveButton("Create", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                // ToDo get user input here
+                                //create an album and add it to the list of albums
+                            }
+                        })
+
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialogBox, int id) {
+                                        dialogBox.cancel();
+                                    }
+                                });
+
+                AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+                alertDialogAndroid.show();
+            }
+        });
 
         /*
         When app starts for first time, albums.dat will not exist
